40026dd9ed65844e31aca0e086ead63f43d315f2
elangeloscuro1 <angeltapia07084759@gmail.com>
2019-12-18 00:16:49 -0800

    2019-12-18 00:16:49 =>

---
+++ bash.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+extensions=(jpg png gif) #final
+
+mainUrl="http://angel07084759.github.io" #final
+
+srcDir="images" #final
+
+
+processImages() #params: 1>srcDir 2>mailUrl 
+{
+	fileOutput=$1/$1".txt" #$1=srcDir
+	>$fileOutput #truncating: file = 0 bytes
+	echo -e "---------------------------"
+	echo -e "SRC_DIR:   "$1
+	echo -e "DEST_FILE: "$fileOutput
+	echo -e "---------------------------"
+
+	for file in $1/*
+	do
+
+		fullPath=$file
+		pathOnly=$(dirname "${fullPath}")
+		fileName=$(basename -- "$fullPath")
+		fileName=${fileName%.*}
+		extension=${fullPath##*.}
+		newFileName=$(echo $fileName | tr -s '[:blank:]' '_')
+		newFileName=$(echo $newFileName | tr '[:punct:]' '_')
+		newFileName=$newFileName.$extension
+		newFullPath=$pathOnly/$newFileName
+		
+		
+		for ext in ${extensions[@]}
+		do
+			if [[ "$ext" == "$(echo "$extension" | tr '[:upper:]' '[:lower:]')" ]]
+			then
+				mv "$fullPath" "$newFullPath"
+				temp=$2/$pathOnly/$newFileName
+				echo $temp
+				echo $temp >> $fileOutput
+				break
+			fi
+		done
+	done
+}
+
+processImages $srcDir $mainUrl
+
+
+read -p "Press [Enter] to continue..."

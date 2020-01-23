7ebe41ec7adda1b179bd4ecff14f3b5174ef6a52
elangeloscuro1 <angeltapia07084759@gmail.com>
2020-01-23 00:59:51 -0800

    2020-01-23 00:59:51 =>

--- _make.sh
+++ _make.sh
@@ -1,24 +1,18 @@
-if [ -n "$(git status --porcelain)" ]; then
-  echo "there are changes";
+date="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
+
+if [ -n "$(git status --porcelain)" ]
+then
+	echo "there are changes!";
+	echo $date>date  
+	echo "Changes Date: $date"
 else
-  echo "no changes";
+	echo "no changes!";
 fi
 
-
 git add .
-
 read -p "Enter message: " userInput
 git commit -m "`date '+%Y-%m-%d %H:%M:%S'` => $userInput"
-
-
-#if (($e == 0 )); then
-date="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
-#echo $date>date
-#fi
 git push
-#echo `git status --porcelain`
-var=`git status --porcelain`
-echo "Changes Date:"
-echo $var
+
 read -p "Press [Enter] to continue..."
 

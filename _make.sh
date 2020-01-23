a330ef925305d940c3bf91113e9fe598f41591dc
elangeloscuro1 <angeltapia07084759@gmail.com>
2020-01-23 00:45:35 -0800

    2020-01-23 00:45:35 =>

--- _make.sh
+++ _make.sh
@@ -4,15 +4,12 @@ e=$?
 read -p "Enter message: " userInput
 git commit -m "`date '+%Y-%m-%d %H:%M:%S'` => $userInput"
 
-e=$(($e + $?))
-if (($e == 0 )); then
+
+#if (($e == 0 )); then
 date="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
-e=$date
-echo $date>date
-fi
-
+#echo $date>date
+#fi
 git push
-
 echo "Changes Date: $e"
 read -p "Press [Enter] to continue..."
 

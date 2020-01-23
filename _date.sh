7d14c26936cd27db5a3db20ac38747dfe329758b
elangeloscuro1 <angeltapia07084759@gmail.com>
2020-01-23 00:12:23 -0800

    2020-01-23 00:12:23 =>

--- _date.sh
+++ _date.sh
@@ -9,5 +9,9 @@ e=$(($e + $?))
 #var="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
 #>data_changed.txt
 #$var > data_changed.txt
+
+a=1
+if (($a > 2)); then echo YES; fi
+
 echo $var
 read -p "ERR:$e"
--- _make.sh
+++ _make.sh
@@ -5,7 +5,7 @@ read -p "Enter message: " userInput
 git commit -m "`date '+%Y-%m-%d %H:%M:%S'` => $userInput"
 git push
 
-if [ "$e" -gt "0" ]; then
+if (($e > 0 )); then
 date="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
 >data_changed.txt
 $date > data_changed.txt
